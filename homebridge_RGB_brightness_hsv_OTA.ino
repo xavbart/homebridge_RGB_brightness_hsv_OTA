5928d35151e1559c0aed3b952623fa3aa3391d9f
Xavier <barthex@gmail.com>
2021-03-10 14:13:07 +0100

    added password for OTA and updated hostname

--- homebridge_RGB_brightness_hsv_OTA.ino
+++ homebridge_RGB_brightness_hsv_OTA.ino
@@ -24,10 +24,10 @@ void setup() {
   // ArduinoOTA.setPort(3232);
 
   // Hostname defaults to esp3232-[MAC]
-  // ArduinoOTA.setHostname("myesp32");
+   ArduinoOTA.setHostname("esp32_1");
 
   // No authentication by default
-  // ArduinoOTA.setPassword("admin");
+   ArduinoOTA.setPassword("adminesp32");
 
   // Password can be set with it's md5 value as well
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
